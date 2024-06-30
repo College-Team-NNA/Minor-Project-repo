6fa7b324d44b78c0fc33eb6d6770659048968991
Naman  Dixit <129660042+dixitnaman@users.noreply.github.com>
2024-06-30 23:58:23 +0530

    Colors_req.dart

--- lib/UI req/Colors_req.dart
+++ lib/UI req/Colors_req.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 class Theme_req {
   static const Color offWhite = Color(0xFFF8F8FD);
   static const Color white = Color(0xFFFFFFFF);
-  static const Color darkgrey = Color(0xFF373737); //name-desig
+  static const Color bio_name = Color(0xFF373737); //name-desig
   static const Color black = Color(0xFF101010);
-  static const Color washedblue = Color(0xFF657994); //information-tags
-  static const Color lightpurple = Color(0xFF9688E9); //continue-follow
-  static const Color offpurple = Color(0xFF9489E9); //pie chart
-  static const Color darkergrey = Color(0xFF1E1E1E); //titles
+  static const Color info_tag_title = Color(0xFF657994); //information-tags
+  static const Color follow_btn = Color(0xFF9688E9); //continue-follow
+  static const Color piechart_outer = Color(0xFF9489E9); //pie chart
+  static const Color titles = Color(0xFF1E1E1E); //titles
 
   
   static const EdgeInsets defaultPadding =
