59f24e22963cbcb93d350a06916c7693698a2b1a
naman3309 <nggoel888@gmail.com>
2024-07-01 00:07:08 +0530

    AppBar Almost Done

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:minor_project/UI%20req/Colors_req.dart';
+import 'package:minor_project/AppBar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,79 +18,6 @@ class _HomeScreenState extends State<HomeScreen> {
         actions: const [AppBars()],
       ),
       body: const Center(),
-    );
-  }
-}
-
-class AppBars extends StatelessWidget {
-  const AppBars({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-        height: 56.0,
-        decoration: const BoxDecoration(color: Theme_req.offWhite),
-        child: Padding(
-          padding: Theme_req.defaultPadding,
-          child: Row(
-            // mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Image.network(
-                'https://s3-alpha-sig.figma.com/img/47f5/5592/238c3c62bcb527a535db7b396f4fcdee?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EEGtEOxrquW0sVeeN~RdedBfmeue1RxYJLP-bC0IXNfBZn71z-FOz6dVdgL7TPxkoDPSlfrFqrdv~whwUQzCZ2cLDQHuW3qKOjGrSKCo6GdAisZ9u6nj8aIn4P8QExCVemQ9EjX3k3UHON6JWs0jp5sM30yevCJSR9baXrxwW08QSrUw-odQweBV7EaIyhl3YaIRoKrAbKPomanL6SNsfdFVlae0W4B~VzJeumlINfL0sPAJ4iX3VID31PjvBcKY1jJik8DiHYJjBdyYE2E0fSPBSkjWfb1Gj4jdUR1fse-dldBP2AlkMgi~Isz2-Sw75JBfvi7wFqGeya-FxgC1bw__',
-              ),
-              const SizedBox(width: 30.0),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Course",
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(width: 10.0),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Discussions",
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(width: 10.0),
-              TextButton(
-                onPressed: () {},
-                child: const Text(
-                  "Pricing",
-                  style: TextStyle(
-                      color: Colors.black, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const Spacer(),
-              const TextField(
-                  // decoration: InputDecoration(
-                  //   fillColor: Theme_req.black,
-                  //   prefixIcon: Icon(Icons.search),
-                  //   prefixIconColor: Colors.white,
-                  // ),
-                  ),
-              const SizedBox(
-                width: 10,
-              ),
-              const CircleAvatar(),
-              const SizedBox(
-                width: 5,
-              ),
-              const CircleAvatar(),
-              const SizedBox(
-                width: 5,
-              ),
-              const CircleAvatar(),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
