687cf58113f8865cbe7c1d321a5fae8ce4acdd9e
naman3309 <nggoel888@gmail.com>
2024-06-30 23:35:40 +0530

    project starting

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minor_project/UI%20req/Colors_req.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -11,7 +12,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFF8F8FD),
+      backgroundColor: Theme_req.offWhite,
       appBar: AppBar(
         actions: const [AppBars()],
       ),
@@ -28,15 +29,17 @@ class AppBars extends StatelessWidget {
     return Expanded(
       child: Container(
         height: 56.0,
-        decoration: BoxDecoration(color: Color(0xFFF8F8FD)),
+        decoration: const BoxDecoration(color: Theme_req.offWhite),
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          padding: Theme_req.defaultPadding,
           child: Row(
+            // mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Image.network(
                 'https://s3-alpha-sig.figma.com/img/47f5/5592/238c3c62bcb527a535db7b396f4fcdee?Expires=1720396800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=EEGtEOxrquW0sVeeN~RdedBfmeue1RxYJLP-bC0IXNfBZn71z-FOz6dVdgL7TPxkoDPSlfrFqrdv~whwUQzCZ2cLDQHuW3qKOjGrSKCo6GdAisZ9u6nj8aIn4P8QExCVemQ9EjX3k3UHON6JWs0jp5sM30yevCJSR9baXrxwW08QSrUw-odQweBV7EaIyhl3YaIRoKrAbKPomanL6SNsfdFVlae0W4B~VzJeumlINfL0sPAJ4iX3VID31PjvBcKY1jJik8DiHYJjBdyYE2E0fSPBSkjWfb1Gj4jdUR1fse-dldBP2AlkMgi~Isz2-Sw75JBfvi7wFqGeya-FxgC1bw__',
               ),
+              const SizedBox(width: 30.0),
               TextButton(
                 onPressed: () {},
                 child: const Text(
@@ -45,6 +48,7 @@ class AppBars extends StatelessWidget {
                       color: Colors.black, fontWeight: FontWeight.bold),
                 ),
               ),
+              const SizedBox(width: 10.0),
               TextButton(
                 onPressed: () {},
                 child: const Text(
@@ -53,6 +57,7 @@ class AppBars extends StatelessWidget {
                       color: Colors.black, fontWeight: FontWeight.bold),
                 ),
               ),
+              const SizedBox(width: 10.0),
               TextButton(
                 onPressed: () {},
                 child: const Text(
@@ -61,9 +66,26 @@ class AppBars extends StatelessWidget {
                       color: Colors.black, fontWeight: FontWeight.bold),
                 ),
               ),
-              Text("Fuck Society 3"),
-              Text("Fuck Society 4"),
-              Text("Fuck Society 5"),
+              const Spacer(),
+              const TextField(
+                  // decoration: InputDecoration(
+                  //   fillColor: Theme_req.black,
+                  //   prefixIcon: Icon(Icons.search),
+                  //   prefixIconColor: Colors.white,
+                  // ),
+                  ),
+              const SizedBox(
+                width: 10,
+              ),
+              const CircleAvatar(),
+              const SizedBox(
+                width: 5,
+              ),
+              const CircleAvatar(),
+              const SizedBox(
+                width: 5,
+              ),
+              const CircleAvatar(),
             ],
           ),
         ),
---
+++ lib/UI req/Colors_req.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Theme_req {
+  static const Color offWhite = Color(0xFFF8F8FD);
+  static const Color black = Color(0xFF101010);
+  static const EdgeInsets defaultPadding =
+      EdgeInsets.symmetric(horizontal: 32.0);
+}
