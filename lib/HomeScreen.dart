ab2537ebebba577273bb62aff0a29a442f06e4c9
naman3309 <nggoel888@gmail.com>
2024-07-01 16:45:50 +0530

    Division Done

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:minor_project/UI%20req/Colors_req.dart';
 import 'package:minor_project/navBar.dart';
@@ -17,7 +18,45 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         actions: const [navBar()],
       ),
-      body: const Center(),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 22.0, horizontal: 28.0),
+          child: Row(
+            children: [
+              Expanded(
+                  flex: 7,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(24.0)),
+                    ),
+                  )),
+              Expanded(
+                  flex: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(24.0)),
+                    ),
+                  )),
+              Expanded(
+                  flex: 9,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(24.0)),
+                    ),
+                  )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
