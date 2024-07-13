42b3453eb66cec6d4c901b141c4b51c4ad1704e7
naman3309 <nggoel888@gmail.com>
2024-07-14 00:27:57 +0530

    merge Responsive

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:minor_project/profile_tab.dart';
+import 'package:minor_project/UI%20files/profile_tab.dart';
 import 'package:minor_project/UI%20req/Colors_req.dart';
 import 'package:minor_project/navBar.dart';
 
