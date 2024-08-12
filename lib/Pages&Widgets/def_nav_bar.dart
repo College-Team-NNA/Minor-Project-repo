135390346897364b22967f12af618aba693faffd
naman3309 <nggoel888@gmail.com>
2024-08-12 23:17:21 +0530

    merged from new

--- lib/Pages&Widgets/def_nav_bar.dart
+++ lib/Pages&Widgets/def_nav_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:minor_project/UI%20req/Colors_req.dart';
+import 'package:minor_project/utils/func.dart';
 
 class DefNavBar extends StatelessWidget {
   const DefNavBar({super.key});
@@ -16,11 +17,15 @@ class DefNavBar extends StatelessWidget {
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Image.network(
-              "https://s3-alpha-sig.figma.com/img/9a2e/d58e/aa7d7d387d8aedeaf7086c75f7aafc8c?Expires=1722816000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=DfE9enMovUN3oToDRbl71KeYop~Pez6XfXbm4pYGZ~UHNHANZlpfX2IunwEyn7Qbhb5eAnvWEcwwIqFcFp4Mc1YFQso5iYJQb4H0jbRSpy3GjK1Rtd8ir1ysEaghTkPLrx7Zn~zjgNeTIE4k59-~TopZohxpQRIH51AzTtQiiIPZNCLyctGftEK-V1AaNWzoUFexCm1kup-q8J33BE9jVXqwzvIzVNVh2yFI5rLPZa9fYo95ShVfoHa4LiQCQiPK7i0tEyos-HANJ3iiAXlRHbkZEZQ~07WQdZ54VQ3iwRlSqCHHBK~Y9eQ1Dqtf9pgFxMtHBkJsMQ81GYzI0yXTAg__",
-              height: 45.0,
-              width: 180.0,
-            ),
+            FutureBuilder(
+                future: link("logo.png"),
+                builder: (context, snapshot) {
+                  return Image.network(
+                    snapshot.data.toString(),
+                    height: 45.0,
+                    width: 180.0,
+                  );
+                }),
             const SizedBox(width: 30.0),
             TextButton(
               onPressed: () {},
--- lib/Pages&Widgets/home.dart
+++ lib/Pages&Widgets/home.dart
@@ -11,7 +11,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(actions: [loggedIn() ? const navBar() : const DefNavBar()]),
+        appBar:
+            AppBar(actions: [loggedIn() ? const navBar() : const DefNavBar()]),
         body: CustomScrollView(
           slivers: [
             // First section: Header and Hero Banner
@@ -19,7 +20,8 @@ class HomePage extends StatelessWidget {
               child: Column(
                 children: [
                   Container(
-                    height: MediaQuery.of(context).size.height * 0.45, // 50% of screen height
+                    height: MediaQuery.of(context).size.height *
+                        0.45, // 50% of screen height
                     width: MediaQuery.of(context).size.width,
                     color: Theme_req.offWhite,
                     child: Column(
@@ -29,11 +31,14 @@ class HomePage extends StatelessWidget {
                           alignment: Alignment.center,
                           children: [
                             RichText(
-                              textAlign: TextAlign.center, // Center-align the text
+                              textAlign:
+                                  TextAlign.center, // Center-align the text
                               text: TextSpan(
                                 style: DefaultTextStyle.of(context)
                                     .style
-                                    .copyWith(color: const Color(0xFF9489E9)), // Default text color
+                                    .copyWith(
+                                        color: const Color(
+                                            0xFF9489E9)), // Default text color
                                 children: [
                                   TextSpan(
                                     text: 'Welcome to\nthe new ',
@@ -69,7 +74,9 @@ class HomePage extends StatelessWidget {
                             ),
                           ],
                         ),
-                        const SizedBox(height: 20,),
+                        const SizedBox(
+                          height: 20,
+                        ),
                         Container(
                           height: MediaQuery.of(context).size.height * 0.05,
                           width: MediaQuery.of(context).size.width * 0.2,
@@ -78,38 +85,42 @@ class HomePage extends StatelessWidget {
                             children: [
                               const Expanded(
                                   child: TextField(
-                                    textAlign: TextAlign.left,
-                                    decoration: InputDecoration(
-                                      hintText: 'Type to search',
-                                      hintStyle: TextStyle(
-                                        color: Color(0xFFACACAC),
-                                        fontSize: 15.0,
-                                      ),
-                                      contentPadding: EdgeInsets.all(15.0),
-                                      border: InputBorder.none,
-                                    ),
-                                  )
-                              ),
+                                textAlign: TextAlign.left,
+                                decoration: InputDecoration(
+                                  hintText: 'Type to search',
+                                  hintStyle: TextStyle(
+                                    color: Color(0xFFACACAC),
+                                    fontSize: 15.0,
+                                  ),
+                                  contentPadding: EdgeInsets.all(15.0),
+                                  border: InputBorder.none,
+                                ),
+                              )),
                               const Spacer(),
                               IconButton(
-                                  onPressed: (){},
+                                  onPressed: () {},
                                   icon: const Icon(Icons.search))
                             ],
                           ),
                         ),
-                        const Text("Tip: Search using tags for better results."),
+                        const Text(
+                            "Tip: Search using tags for better results."),
                       ],
                     ),
                   ),
-                  LayoutBuilder(builder: (context, constraints) {
-                    return CustomPaint(
-                      size: Size(constraints.maxWidth * 0.7, 7), // 70% of screen width
-                      painter: TaperedDividerPainter(),
-                    );
-                    },),
+                  LayoutBuilder(
+                    builder: (context, constraints) {
+                      return CustomPaint(
+                        size: Size(constraints.maxWidth * 0.7,
+                            7), // 70% of screen width
+                        painter: TaperedDividerPainter(),
+                      );
+                    },
+                  ),
                   Container(
-                    padding: EdgeInsets.symmetric(horizontal: 15,vertical: 10),
-                    height: MediaQuery.of(context).size.height * 0.5, // 50% of screen height
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    height: MediaQuery.of(context).size.height *
+                        0.5, // 50% of screen height
                     color: Theme_req.offWhite,
                     child: Column(
                       children: [
@@ -117,30 +128,34 @@ class HomePage extends StatelessWidget {
                           // mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Expanded(
-                            child: SingleChildScrollView(
-                            scrollDirection: Axis.horizontal,
+                                child: SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
                               child: Wrap(
-                              children: [
-                                CategoryButton(label: 'All', isSelected: true),
-                                CategoryButton(label: 'UI/UX'),
-                                CategoryButton(label: 'Neo Brutalism'),
-                                CategoryButton(label: 'Minimal'),
-                                CategoryButton(label: 'Maximal'),
-                                CategoryButton(label: 'Retro'),
-                                CategoryButton(label: 'Heat Map'),
-                                CategoryButton(label: 'Typography'),
-                                CategoryButton(label: 'Package Design'),
-                                CategoryButton(label: 'AI Generated'),
-                            ],
-                            ),
-                            )
-                          ),
+                                children: [
+                                  CategoryButton(
+                                      label: 'All', isSelected: true),
+                                  CategoryButton(
+                                    label: 'UI/UX',
+                                  ),
+                                  CategoryButton(label: 'Neo Brutalism'),
+                                  CategoryButton(label: 'Minimal'),
+                                  CategoryButton(label: 'Maximal'),
+                                  CategoryButton(label: 'Retro'),
+                                  CategoryButton(label: 'Heat Map'),
+                                  CategoryButton(label: 'Typography'),
+                                  CategoryButton(label: 'Package Design'),
+                                  CategoryButton(label: 'AI Generated'),
+                                ],
+                              ),
+                            )),
                             Row(
                               children: [
                                 OutlinedButton(
                                   onPressed: null,
                                   style: OutlinedButton.styleFrom(
-                                    padding: EdgeInsets.symmetric(horizontal: 2.0, vertical: 0.0), // Reduced padding
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 2.0,
+                                        vertical: 0.0), // Reduced padding
                                     minimumSize: Size(10, 30),
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(5.0),
@@ -153,7 +168,8 @@ class HomePage extends StatelessWidget {
                                   child: DropdownButtonHideUnderline(
                                     child: DropdownButton<String>(
                                       icon: const Icon(Icons.filter_list),
-                                      iconSize: 15, // Reduce the size of the icon
+                                      iconSize:
+                                          15, // Reduce the size of the icon
                                       iconEnabledColor: Theme_req.black,
                                       items: const [
                                         DropdownMenuItem<String>(
@@ -178,8 +194,10 @@ class HomePage extends StatelessWidget {
                                           fontSize: Theme_req.stxt_size,
                                         ),
                                       ),
-                                      isDense: true, // Make the dropdown more compact
-                                      alignment: Alignment.center, // Align the text in the center
+                                      isDense:
+                                          true, // Make the dropdown more compact
+                                      alignment: Alignment
+                                          .center, // Align the text in the center
                                     ),
                                   ),
                                 ),
@@ -187,7 +205,9 @@ class HomePage extends StatelessWidget {
                                 OutlinedButton(
                                   onPressed: null,
                                   style: OutlinedButton.styleFrom(
-                                    padding: EdgeInsets.symmetric(horizontal: 2.0, vertical: 0.0), // Reduced padding
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 2.0,
+                                        vertical: 0.0), // Reduced padding
                                     minimumSize: Size(10, 30),
                                     shape: RoundedRectangleBorder(
                                       borderRadius: BorderRadius.circular(5.0),
@@ -200,7 +220,8 @@ class HomePage extends StatelessWidget {
                                   child: DropdownButtonHideUnderline(
                                     child: DropdownButton<String>(
                                       icon: const Icon(Icons.expand_more),
-                                      iconSize: 15, // Reduce the size of the icon
+                                      iconSize:
+                                          15, // Reduce the size of the icon
                                       iconEnabledColor: Theme_req.black,
                                       items: const [
                                         DropdownMenuItem<String>(
@@ -225,8 +246,10 @@ class HomePage extends StatelessWidget {
                                           fontSize: Theme_req.stxt_size,
                                         ),
                                       ),
-                                      isDense: true, // Make the dropdown more compact
-                                      alignment: Alignment.center, // Align the text in the center
+                                      isDense:
+                                          true, // Make the dropdown more compact
+                                      alignment: Alignment
+                                          .center, // Align the text in the center
                                     ),
                                   ),
                                 ),
@@ -234,16 +257,19 @@ class HomePage extends StatelessWidget {
                             ),
                           ],
                         ),
-                        SizedBox(height: 20,),
+                        SizedBox(
+                          height: 20,
+                        ),
                         Expanded(
                           child: GridView.builder(
                             shrinkWrap: true,
                             itemCount: 4,
-                            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 4,
-                                crossAxisSpacing: 16,
-                                mainAxisSpacing: 16,
-                                childAspectRatio: 16 / 10),
+                            gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                    crossAxisCount: 4,
+                                    crossAxisSpacing: 16,
+                                    mainAxisSpacing: 16,
+                                    childAspectRatio: 16 / 10),
                             itemBuilder: (BuildContext context, int index) {
                               return Container(
                                 decoration: BoxDecoration(
@@ -255,11 +281,13 @@ class HomePage extends StatelessWidget {
                                     padding: const EdgeInsets.all(12),
                                     child: Container(
                                       decoration: BoxDecoration(
-                                          borderRadius: BorderRadius.circular(22)),
+                                          borderRadius:
+                                              BorderRadius.circular(22)),
                                       clipBehavior: Clip.antiAlias,
                                       child: FutureBuilder(
-                                        future:
-                                        (index % 2 == 0) ? link("1.png") : link("2.png"),
+                                        future: (index % 2 == 0)
+                                            ? link("1.png")
+                                            : link("2.png"),
                                         builder: (context, snapshot) {
                                           if (snapshot.connectionState ==
                                               ConnectionState.done) {
@@ -269,7 +297,8 @@ class HomePage extends StatelessWidget {
                                             );
                                           } else {
                                             return const Center(
-                                                child: CircularProgressIndicator());
+                                                child:
+                                                    CircularProgressIndicator());
                                           }
                                         },
                                       ),
@@ -289,12 +318,14 @@ class HomePage extends StatelessWidget {
               child: Column(
                 children: [
                   Container(
-                    height: MediaQuery.of(context).size.height * 0.5, // 50% of screen height
+                    height: MediaQuery.of(context).size.height *
+                        0.5, // 50% of screen height
                     color: Colors.black,
                     // child: WeeklyTopDesignsWidget(), // Replace with your actual WeeklyTopDesignsWidget
                   ),
                   Container(
-                    height: MediaQuery.of(context).size.height * 0.5, // 50% of screen height
+                    height: MediaQuery.of(context).size.height *
+                        0.5, // 50% of screen height
                     color: Colors.white,
                     // child: FreshmenWidget(), // Replace with your actual FreshmenWidget
                   ),
@@ -306,13 +337,15 @@ class HomePage extends StatelessWidget {
               child: Column(
                 children: [
                   Container(
-                    height: MediaQuery.of(context).size.height * 0.3, // 30% of screen height
+                    height: MediaQuery.of(context).size.height *
+                        0.3, // 30% of screen height
                     color: Colors.black,
                     // child: TopArtistsWidget(), // Replace with your actual TopArtistsWidget
                   ),
                   const Divider(),
                   Container(
-                    height: MediaQuery.of(context).size.height * 0.7, // 70% of screen height
+                    height: MediaQuery.of(context).size.height *
+                        0.7, // 70% of screen height
                     color: Colors.black,
                     // child: FooterWidget(), // Replace with your actual FooterWidget
                   ),
@@ -320,8 +353,7 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ],
-        )
-    );
+        ));
   }
 }
 
@@ -336,18 +368,23 @@ class CategoryButton extends StatelessWidget {
     return OutlinedButton(
       style: OutlinedButton.styleFrom(
         foregroundColor: isSelected ? Colors.white : Theme_req.black,
-        backgroundColor: isSelected ? const Color(0xFF9489E9) : Colors.transparent,
+        backgroundColor:
+            isSelected ? const Color(0xFF9489E9) : Colors.transparent,
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(8.0),
         ),
-        side: isSelected ? BorderSide.none : const BorderSide(color: Colors.transparent),
+        side: isSelected
+            ? BorderSide.none
+            : const BorderSide(color: Colors.transparent),
       ),
       onPressed: () {},
-      child: Text(label,style: GoogleFonts.montserrat(fontWeight: FontWeight.w500, fontSize: Theme_req.stxt_size)
-      ),
+      child: Text(label,
+          style: GoogleFonts.montserrat(
+              fontWeight: FontWeight.w500, fontSize: Theme_req.stxt_size)),
     );
   }
 }
+
 class TaperedDividerPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
@@ -359,7 +396,10 @@ class TaperedDividerPainter extends CustomPainter {
     // Create a path that starts thin at the edges and thickens towards the center
     Path path = Path();
     for (double i = 0; i < size.width; i++) {
-      double thickness = 6.0 * (1 - ((i - size.width / 2).abs() / (size.width / 2))); // Adjust thickness
+      double thickness = 6.0 *
+          (1 -
+              ((i - size.width / 2).abs() /
+                  (size.width / 2))); // Adjust thickness
       path.moveTo(i, size.height / 2 - thickness / 2);
       path.lineTo(i, size.height / 2 + thickness / 2);
     }
